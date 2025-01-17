ecd3fd6f2fc6042161d54b0385178d1fc20e70a1
Michael Shpilt <contact@obics.io>
2025-01-17 09:31:49 +0200

    traces instrumentation

--- otel-setup.js
+++ otel-setup.js
@@ -1,3 +1,5 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable lines-around-directive */
 // otel-setup.js
 'use strict';
 
@@ -6,14 +8,24 @@ const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
-
-
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 // 1. Create a tracer provider
-const provider = new NodeTracerProvider();
+const provider = new NodeTracerProvider({
+	resource: new Resource({
+		[SemanticResourceAttributes.SERVICE_NAME]: 'nodebb', // Replace with your service name
+	}),
+});
+
+
+
 
 // 2. Configure your exporter
 const exporter = new OTLPTraceExporter({
-  // e.g. url: 'http://localhost:4318/v1/traces'
+	url: 'https://ingest.obics.io/api/otel/v1/traces', // 'http://localhost:5183/api/otel/v1/traces',
+	headers: {
+		'x-api-key': '0f67a886-7f3b-4d60-a206-9673d584118f', // sandbox key
+	},
 });
 
 // 3. Add the span processor
@@ -24,8 +36,8 @@ provider.register();
 
 // 5. Register auto-instrumentations
 registerInstrumentations({
-  tracerProvider: provider,
-  instrumentations: [ getNodeAutoInstrumentations() ],
+	tracerProvider: provider,
+	instrumentations: [getNodeAutoInstrumentations()],
 });
 
 // No need to require loader.js here—Node will load this file first,
